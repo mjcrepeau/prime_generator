38cfc91d10d7e085effbc7280cc76487242c3acd
mjcrepeau <matthew.mjc@gmail.com>
2015-12-07 00:25:03 -0700

    Updated comments.

--- primes.swift
+++ primes.swift
@@ -1,6 +1,6 @@
 /*
 Written by Matthew Crepeau
-12/3/2015
+12/7/2015
 Calculates the prime numbers between 2 and n
 */
 import Darwin   // Needed to call exit()
@@ -17,7 +17,7 @@ if Process.arguments.count != 2 {
 }
 
 var primes: [Int] = []          // Array to hold the primes
-for var i = 1; i < n; i+=2 {     // For the second refinement, start with odd numbers only
+for var i = 1; i < n; i+=2 {    // For the second refinement, start with odd numbers only
         primes.append(i)        // Step 1 (see readme for steps)
 }
 
@@ -30,13 +30,13 @@ while p*p < n { // p*p is part of the first refinement
                 j += 2;
         }
         p = p + j
-        for var i = p; p*i < n; i+=2 {   // Step 3 (set i = p for the first refinement, increment by 2p for second refinement)
+        for var i = p; p*i < n; i+=2 {  // Step 3 (set i = p for the first refinement, increment by 2p for second refinement)
                 primes[(p*i)/2] = 0     // Mark non-primes as 0
         }
 }
 
-// Print the non-marked numbers
 print("2")      // manually print the only even prime
+// Print the non-marked numbers
 for var i = 3; i < n; i+=2 {
         if primes[(i/2)] != 0 {
                 print("\(primes[(i/2)])")
