fe309422df8c2c8fce4916ce914e9b4241e63c29
mjcrepeau <matthew.mjc@gmail.com>
2015-12-07 00:13:05 -0700

    Implemented the second refinement.

--- primes.swift
+++ primes.swift
@@ -17,7 +17,7 @@ if Process.arguments.count != 2 {
 }
 
 var primes: [Int] = []          // Array to hold the primes
-for var i = 0; i < n; i++ {
+for var i = 1; i < n; i+=2 {     // For the second refinement, start with odd numbers only
         primes.append(i)        // Step 1 (see readme for steps)
 }
 
@@ -26,18 +26,19 @@ var p = 1       // Step 2
 
 while p*p < n { // p*p is part of the first refinement
         j = 0
-        while p+j < n && (primes[p + j] <= p || primes[p + j] == 0) {   // Step 4
-                j++
+        while p+j < n && (primes[(p + j)/2] <= p || primes[(p + j)/2] == 0) {   // Step 4
+                j += 2;
         }
         p = p + j
-        for var i = p; p*i < n; i++ {   // Step 3 (set i = p for the first refinement)
-                primes[p*i] = 0         // Mark non-primes as 0
+        for var i = p; p*i < n; i+=2 {   // Step 3 (set i = p for the first refinement, increment by 2p for second refinement)
+                primes[(p*i)/2] = 0     // Mark non-primes as 0
         }
 }
 
 // Print the non-marked numbers
-for var i = 2; i < n; i++ {
-        if primes[i] != 0 {
-                print("\(primes[i])")
+print("2")      // manually print the only even prime
+for var i = 3; i < n; i+=2 {
+        if primes[(i/2)] != 0 {
+                print("\(primes[(i/2)])")
         }
 }
